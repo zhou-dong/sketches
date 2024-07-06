539855197e4335201c55d73601bfe77b24ba8a2f
DONG ZHOU <82224165@qq.com>
2024-07-06 11:01:51 -0700

    Update ThetaSketch.scala

--- src/main/scala/com.alchemist_al.sketches/theta_sketch/ThetaSketch.scala
+++ src/main/scala/com.alchemist_al.sketches/theta_sketch/ThetaSketch.scala
@@ -1,3 +1,28 @@
 package com.alchemist_al.sketches.theta_sketch
 
-class ThetaSketch {}
+import scala.collection.mutable
+
+class ThetaSketch {
+
+  private val sketchSet: mutable.Set[Long] = mutable.Set()
+
+  def add(element: Any): Unit =
+    sketchSet += element.hashCode.toLong
+
+  def estimate: Long =
+    sketchSet.size.toLong
+
+  def union(other: ThetaSketch): ThetaSketch =
+    val unionSketch = new ThetaSketch
+    unionSketch.sketchSet ++= this.sketchSet
+    unionSketch.sketchSet ++= other.sketchSet
+    unionSketch
+
+  def intersect(other: ThetaSketch): ThetaSketch =
+    val intersectionSketch = new ThetaSketch
+    for (value <- this.sketchSet)
+      if (other.sketchSet.contains(value))
+        intersectionSketch.sketchSet.add(value)
+    intersectionSketch
+
+}
